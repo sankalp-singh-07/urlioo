bff79741044b8a8bda7d7c32a2b1e9cba397fe11
sankalp-singh-07 <sankalp.singh.somvanshi@gmail.com>
2025-05-20 21:44:38 +0530

    Qr code and link shortening complete for first part

--- script.js
+++ script.js
@@ -4,14 +4,29 @@
 const Form = document.querySelector('.form');
 const Input = document.querySelector('.input');
 const submitBtn = document.querySelector('.shortURL');
+const qrImg = document.getElementById('imgqr');
+const qrsec = document.querySelector('.qrSec');
+const urlShort = document.querySelector('.urlshort');
+const copyBtnQr = document.querySelector('.copyurl');
+const loadingUrl = document.querySelector('.loadingUrl');
+
+let linkShort = '';
 
 Form.addEventListener('submit', (e) => {
 	e.preventDefault();
 	const link = Input.value;
-	if (checkUrl(link)) {
-		getLink(link);
-		alert('success');
-	} else console.log(false);
+	try {
+		if (checkUrl(link)) {
+			if (checkhttps(link)) {
+				getLink(link);
+			} else {
+				const s = 'http://' + link;
+				getLink(s);
+			}
+		} else return false;
+	} catch (error) {
+		console.log(error);
+	}
 });
 
 const dataArr = {
@@ -20,7 +35,9 @@ const dataArr = {
 	views: '',
 };
 
-const getLink = async (link) => {
+const checkhttps = (link) => link.startsWith('http');
+
+const getLink = async (linkLong) => {
 	const res = await fetch('https://api-ssl.bitly.com/v4/shorten', {
 		method: 'POST',
 		headers: {
@@ -28,15 +45,54 @@ const getLink = async (link) => {
 			'Content-Type': 'application/json',
 		},
 		body: JSON.stringify({
-			long_url: link,
+			long_url: linkLong,
 		}),
 	});
+	if (!res.ok) throw new Error('Bitly Error');
 	const data = await res.json();
-	console.log(data);
+	const { link, created_at } = data;
+	linkShort = link;
+
+	//> showing loading
+	submitBtn.textContent = 'Generating...';
+	loadingUrl.classList.remove('hid');
+	setTimeout(() => {
+		loadingUrl.classList.add('hid');
+		qrsec.classList.remove('hid');
+		submitBtn.textContent = 'Generate Link';
+	}, 3000);
+
+	//>generate qr
+	const qrCode = generateQRCode(link);
+	qrImg.src = qrCode;
+
+	urlShort.textContent = link;
 };
+
+urlShort.addEventListener('click', () => {
+	window.open(linkShort, '_blank');
+});
+
+copyBtnQr.addEventListener('click', () => {
+	navigator.clipboard
+		.writeText(linkShort)
+		.then(() => {
+			copyBtnQr.textContent = 'Copied';
+			setTimeout(() => {
+				copyBtnQr.textContent = 'Copy Link';
+			}, 3000);
+		})
+		.catch((error) => alert(error));
+});
 
 const urlPattern = /^(https?:\/\/)?([\w\-]+(\.[\w\-]+)+)(\/[\w\-./?%&=]*)?$/i;
 
 const checkUrl = (url) => {
 	return urlPattern.test(url);
+};
+
+const generateQRCode = (link) => {
+	return `https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(
+		link
+	)}&size=200x200`;
 };
