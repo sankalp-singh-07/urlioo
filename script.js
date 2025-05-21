1f0a4d5ad2349be85f9d736811d89a1d0b8eeac9
sankalp-singh-07 <sankalp.singh.somvanshi@gmail.com>
2025-05-21 22:02:56 +0530

    Basic structure completed for website only show more left

--- script.js
+++ script.js
@@ -12,9 +12,18 @@ const loadingUrl = document.querySelector('.loadingUrl');
 
 let linkShort = '';
 
+const curr = {
+	old: '',
+	new: '',
+	time: new Date().getTime(),
+};
+
+let arr = JSON.parse(localStorage.getItem('shortLinks')) ?? [];
+
 Form.addEventListener('submit', (e) => {
 	e.preventDefault();
 	const link = Input.value;
+	curr.old = link;
 	try {
 		if (checkUrl(link)) {
 			if (checkhttps(link)) {
@@ -29,19 +38,13 @@ Form.addEventListener('submit', (e) => {
 	}
 });
 
-const dataArr = {
-	url: '',
-	old: '',
-	views: '',
-};
-
 const checkhttps = (link) => link.startsWith('http');
 
 const getLink = async (linkLong) => {
 	const res = await fetch('https://api-ssl.bitly.com/v4/shorten', {
 		method: 'POST',
 		headers: {
-			Authorization: 'Bearer 2e467fcac664f108511ac60172ca86d99d589f83',
+			Authorization: 'Bearer ac97dfccc2169fececa29173535c199e6584aed1',
 			'Content-Type': 'application/json',
 		},
 		body: JSON.stringify({
@@ -51,10 +54,12 @@ const getLink = async (linkLong) => {
 	if (!res.ok) throw new Error('Bitly Error');
 	const data = await res.json();
 	const { link, created_at } = data;
+	curr.new = link;
 	linkShort = link;
 
 	//> showing loading
 	submitBtn.textContent = 'Generating...';
+	qrsec.classList.add('hid');
 	loadingUrl.classList.remove('hid');
 	setTimeout(() => {
 		loadingUrl.classList.add('hid');
@@ -67,6 +72,7 @@ const getLink = async (linkLong) => {
 	qrImg.src = qrCode;
 
 	urlShort.textContent = link;
+	arrUpdate({ ...curr });
 };
 
 urlShort.addEventListener('click', () => {
@@ -96,3 +102,91 @@ const generateQRCode = (link) => {
 		link
 	)}&size=200x200`;
 };
+
+//> Previous links
+function arrUpdate(curr) {
+	arr.push(curr);
+
+	console.log(arr);
+
+	localStorage.setItem('shortLinks', JSON.stringify(arr));
+
+	const trimmed = arr.slice(0, 3);
+	console.log(trimmed);
+}
+
+//> Previous Links shown
+const wrapper = document.querySelector('.links-wrapper');
+const existing = JSON.parse(localStorage.getItem('shortLinks')) || [];
+existing.forEach((ele) => {
+	const prev = document.createElement('div');
+	prev.classList.add('link-item');
+	prev.innerHTML = `
+		<p class="original-link">${ele.old}</p>
+		<div class="link-details">
+			<span class="short-link">${ele.new}</span>
+			<div class="copyPart">
+				<button class="copy-btn" data-copy="original">Copy Original</button>
+				<button class="copy-btn" data-copy="short">Copy Short</button>
+				<span class="delete">X</span>
+			</div>
+		</div>
+	`;
+
+	wrapper.appendChild(prev);
+});
+
+wrapper.addEventListener('click', (e) => {
+	if (e.target.classList.contains('copy-btn')) {
+		const btn = e.target;
+		const dataset = btn.dataset.copy;
+		const linkItem = btn.closest('.link-item');
+		const text =
+			dataset === 'original'
+				? linkItem.querySelector('.original-link').textContent
+				: linkItem.querySelector('.short-link').textContent;
+
+		navigator.clipboard
+			.writeText(text)
+			.then(() => {
+				btn.textContent = 'Copied';
+				setTimeout(() => {
+					btn.textContent =
+						dataset === 'original' ? 'Copy Original' : 'Copy Short';
+				}, 2000);
+			})
+			.catch((err) => {
+				alert('Copy failed: ' + err);
+			});
+	}
+});
+
+wrapper.addEventListener('click', (e) => {
+	if (e.target.classList.contains('delete')) {
+		const btn = e.target;
+		const linkItem = btn.closest('.link-item');
+		const shortLink = linkItem.querySelector('.short-link').textContent;
+		// console.log(shortLink);
+		linkItem.remove();
+
+		const newArr = arr.filter((item) => item.new != shortLink);
+		localStorage.setItem('shortLinks', JSON.stringify(newArr));
+	}
+});
+
+//> Field focus
+const inputField = document.getElementById('inputField');
+document.querySelector('.footerBTN').addEventListener('click', () => {
+	inputField.focus();
+	document.querySelector('.navbar').scrollIntoView({
+		behavior: 'smooth',
+		block: 'start',
+	});
+});
+
+document.querySelector('.links').addEventListener('click', () => {
+	document.querySelector('.prev-links-section').scrollIntoView({
+		behavior: 'smooth',
+		block: 'start',
+	});
+});
